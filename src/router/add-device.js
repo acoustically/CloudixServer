d0fdfc3b44dded899a252250b8787f10496f26a5
Ubuntu <ubuntu@ip-172-31-3-223.ap-northeast-2.compute.internal>
2017-10-04 11:38:59 +0000

    bugfix in check device work

--- src/router/add-device.js
+++ src/router/add-device.js
@@ -9,8 +9,8 @@ module.exports = function addDeviceRouter(app) {
     let user_id = req.body.user_id;
     let serial = req.body.serial;
     let password = req.body.password;
-    let error = `Error: check device    user_id : ${user_id} serial : ${serial} device password : ${password}`;
-    let success = `Success: check device    user_id : ${user_id} serial : ${serial} device password : ${password}`;
+    let error = `Error: check device  /  user_id : ${user_id}, serial : ${serial}, device password : ${password}`;
+    let success = `Success: check device  /  user_id : ${user_id}, serial : ${serial}, device password : ${password}`;
     
     let rdsConnector = new RDSConnector();
     sql = `select * from switchs where serial="${serial}"`;
@@ -29,7 +29,7 @@ module.exports = function addDeviceRouter(app) {
           if(result[0].password == null) { // if new password is not exist
             if(result[0].original_password == password) { //if password is equal to original password
               logger.log(success);
-              res.json({"response":"success", "isOriginalPasswordDirty":"false"});
+              res.json({"response":"success", "isOriginalPasswordDirty":false});
             } else { //if password is not equal
               logger.log(error);
               logger.log("\t wrong password");
@@ -38,7 +38,7 @@ module.exports = function addDeviceRouter(app) {
           } else { // if new password is exist
             if(result[0].password == password) { // if password is equal to new password
               logger.log(success);
-              res.json({"response":"success", "isOriginalPasswordDirty":"true"});
+              res.json({"response":"success", "isOriginalPasswordDirty":true});
             } else { // if password is not equal to new password
               if(result[0].original_password == password) { //if password is equal to original password
                 logger.log(error);
